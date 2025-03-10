8804ba25303912b72266a72a116d28c44d96811f
Paintballskaguy <Johnstournament@gmail.com>
2025-03-10 12:25:24 -0500

    task 9 - groceries!

---
+++ ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  // create a new Map with specified things from the list //
+  return new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasta', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+}
