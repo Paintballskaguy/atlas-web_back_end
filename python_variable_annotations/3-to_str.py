be4bf3150af00d56dac53c1c67e8d76c4320d8f4
Paintballskaguy <Johnstournament@gmail.com>
2025-05-12 13:55:17 -0500

    task 3 - basic annotations.

---
+++ python_variable_annotations/3-to_str.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+Converts a float to a string. uses function to_str.
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Converts a float to a string. uses function to_str.
+    Args:
+        n (float): float-point number to be converted
+
+    Returns:
+        str: string that represents the float.
+    """
+    return str(n)
