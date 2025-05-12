c1734b380754e484583683081100786816330592
Paintballskaguy <Johnstournament@gmail.com>
2025-05-12 16:05:24 -0500

    : instead of ;

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -8,7 +8,7 @@ This function returns a tuple with a string and squared number.
 from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float];
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """This function returns a tuple with a string and squared number.
 
     Returns:
