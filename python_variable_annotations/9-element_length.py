c963b18be6ae33cb42c8d434f6bcf47e99ebd610
Paintballskaguy <Johnstournament@gmail.com>
2025-05-12 16:20:44 -0500

    task 10 - 1st is not lst

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""
+This module provides a function that returns element lengths in an iterable.
+The module contains a single function 'element_length' that takes an iterable
+of sequences and returns a list of tuples containing each element and its length.
+"""
+
+
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Takes an iterable of sequences and returns a list of tuples with each element and its length.
+
+    Args:
+        lst: An iterable containing sequence-like elements (strings, lists, etc.)
+
+    Returns:
+        A list of tuples where each tuple contains (element, element_length)
+    """
+    return [(i, len(i)) for i in lst]
