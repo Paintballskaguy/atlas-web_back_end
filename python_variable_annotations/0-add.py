ce83c7e60fd00aa8e08ed5cd46adf35bd1a6b8ec
Paintballskaguy <Johnstournament@gmail.com>
2025-05-12 12:13:05 -0500

    documentation

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -7,4 +7,13 @@ This adds two floats together.
 
 
 def add(a: float, b: float) -> float:
+    """_summary_
+
+    Args:
+        a (float): float number
+        b (float): float number
+
+    Returns:
+        float: total of the floats
+    """
     return a + b
