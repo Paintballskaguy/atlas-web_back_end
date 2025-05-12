d842d7dbf55cef90db0c8d892e133e845157e108
Paintballskaguy <Johnstournament@gmail.com>
2025-05-12 16:12:03 -0500

    task 9 - float multiplier.

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""
+Multiplier of a float and returns a multiple float.
+"""
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """
+    Multiplier of a float and returns a multiple float.
+
+    Args:
+        multiplier (float): the float value to multiply by.
+
+    Returns:
+        Callable[[float], float]: returns the product of the math
+    """
+    
+    def multiplier_function(x: float) -> float:
+        return x * multiplier
+    
+    return multiplier_function
