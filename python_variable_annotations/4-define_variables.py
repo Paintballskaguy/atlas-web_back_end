376f394400f72ed6aeb2d63c4da7c25ee2723d6f
Paintballskaguy <Johnstournament@gmail.com>
2025-05-12 13:58:29 -0500

    task 4 - defining variables

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+This module defines and annotates variables with specific values.
+- a: integer
+- pi: float
+- i_understand_annotations: boolean
+- school: string
+"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
