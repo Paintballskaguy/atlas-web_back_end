c1f48ed255f81758fe148178a0e8fd7d82935a71
Paintballskaguy <Johnstournament@gmail.com>
2025-05-12 12:20:01 -0500

    task 1 - concat stuff

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/bin/env/ python3
+
+"""
+Concate two strings together is just smashing two words together.
+"""
+    
+def concat(str1: str, str2: str) -> str:
+        """_summary_
+
+    Args:
+        str1 (str): 1st one to be stuck together
+        str2 (str): 2nd one,the tailend
+
+    Returns:
+        str: str1+str2  = str3.
+        """
+        return str1 + str2
