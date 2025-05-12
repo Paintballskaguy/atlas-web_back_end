b2d72818603fdd631e4adc33a043eae683ea439f
Paintballskaguy <Johnstournament@gmail.com>
2025-05-12 16:04:24 -0500

    task 8 - tuple t6 float

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+"""
+This function returns a tuple with a string and squared number.
+"""
+
+
+from typing import Union, Tuple
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float];
+    """This function returns a tuple with a string and squared number.
+
+    Returns:
+        _type_: tuple containing the string k and the square v.
+    """
+    return (k, float(v ** 2))
