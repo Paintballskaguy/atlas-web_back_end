e6bb58bc9d2998f2576acaf987f14f395f2c616e
Paintballskaguy <Johnstournament@gmail.com>
2025-05-12 12:21:43 -0500

    pycode again

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -3,9 +3,10 @@
 """
 Concate two strings together is just smashing two words together.
 """
-    
+
+
 def concat(str1: str, str2: str) -> str:
-        """_summary_
+    """_summary_
 
     Args:
         str1 (str): 1st one to be stuck together
@@ -13,5 +14,5 @@ def concat(str1: str, str2: str) -> str:
 
     Returns:
         str: str1+str2  = str3.
-        """
-        return str1 + str2
+    """
+    return str1 + str2
