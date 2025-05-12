bf8265e5a9f8dadb3aae00ef72025d74d03a172b
Paintballskaguy <Johnstournament@gmail.com>
2025-05-12 14:01:38 -0500

    task 5 - list of floats.

---
+++ python_variable_annotations/5-sum_list.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+"""
+ Listing out the floats.
+"""
+
+
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    """_summary_
+
+    Args:
+        input_list (List[float]): A list containing floating-point numbers.
+
+    Returns:
+        float: sum of all the elements in the input
+    """
+    return sum(input_list)
