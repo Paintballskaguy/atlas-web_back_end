0ffbbc70a159a174c4fe7015608429037abd2216
Paintballskaguy <Johnstournament@gmail.com>
2025-05-14 13:30:49 -0500

    pycode, again

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -3,22 +3,21 @@
 
 """
 Module that contains a coroutine that measures the runtime
-of async_comprehension four times in parallel.    
+of async_comprehension four times in parallel.
 """
 
 
 import asyncio
 import time
-from typing import Coroutine
+from typing import List, Coroutine
 
 
 async def measure_runtime() -> float:
-    
     """
     Executes 4 async_comprehensions in parallel and measures runtime.
 
     Returns:
-        _type_: Total runtime in seconds.
+        float: Total runtime in seconds.
     """
     async_comprehension = __import__('1-async_comprehension').async_comprehension
     start_time = time.time()
