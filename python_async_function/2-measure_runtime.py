91ba6734769776186b93dabd39d7a2ed87b8c3cc
Paintballskaguy <Johnstournament@gmail.com>
2025-05-14 11:14:35 -0500

    task 3 - measuring time for execution.

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+
+"""
+measuring that time. measure the time for execution time per task.
+"""
+
+
+import asyncio
+import time
+from typing import Callable
+
+
+wait_n = __import__('1-concurrent_coroutines'). wait_n
+
+
+def measure_time(n: int, max_delay: int) ->  float:
+    """measure the time for execution time per task.
+
+    Args:
+        n (int):  Number of times to spawn wait_random
+        max_delay (int): Maximum delay value for wait_random.
+
+    Returns:
+        float: Average execution time per task.
+    """
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    total_time = end_time - start_time
+    return total_time / n
