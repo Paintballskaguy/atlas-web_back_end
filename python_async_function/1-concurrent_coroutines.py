92bf38f8f05e3670acb6fc00761d342212884000
Paintballskaguy <Johnstournament@gmail.com>
2025-05-14 11:09:00 -0500

    pycode

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-    
+
 """
 wait_random n times with a specific max_delay and returns
 the list of delays
@@ -12,6 +12,7 @@ from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
+
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """wait_random n times with a specific max_delay and returns
