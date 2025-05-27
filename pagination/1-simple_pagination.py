0685a34f0348a5556e0727f5255ca62ff7bb0f37
Paintballskaguy <Johnstournament@gmail.com>
2025-05-27 13:44:22 -0500

    added doc strings

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -26,6 +26,8 @@ def index_range(page: int, page_size: int) -> tuple:
 
 
 class Server:
+    """Server class to paginate a database of popular baby names.
+    """
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
@@ -43,7 +45,16 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Retrieve a page of data from the dataset.
 
+        Args:
+            page (int, optional): The page number (1-indexed)
+            page_size (int, optional): The number of items per page
+
+        Returns:
+            List[List]:
+            The requested page of data or empty list if out of range.
+        """
         assert isinstance(page, int) and page > 0, (
             "page must be a positive integer")
         assert isinstance(page_size, int) and page_size > 0, (
