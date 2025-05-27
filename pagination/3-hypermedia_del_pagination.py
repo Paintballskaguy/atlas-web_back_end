79d665e9b1e454539e98c3084a4e8be2cf0ab283
Paintballskaguy <Johnstournament@gmail.com>
2025-05-27 14:56:57 -0500

    pycode again

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -39,10 +39,8 @@ class Server:
         return self.__indexed_dataset
 
     def get_hyper_index(
-        self,
-        index: int = None,
-        page_size: int = 10
-        ) -> Dict[str, Optional[Union[int, List[List]]]]:
+        self, index: int = None, page_size: int = 10)
+        -> Dict[str, Optional[Union[int, List[List]]]]:
         """Get deletion-resilient hypermedia pagination info.
 
         Args:
