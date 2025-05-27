b2ec15d713ba36c7d867e7cd8581839b01d6ac74
Paintballskaguy <Johnstournament@gmail.com>
2025-05-27 13:42:41 -0500

    fixed pycode

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -44,10 +44,10 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
 
-        assert isinstance(page, int) and page > 0,
-        "page must be a positive integer"
-        assert isinstance(page_size, int) and page_size > 0,
-        "page_size must be a positive integer"
+        assert isinstance(page, int) and page > 0, (
+            "page must be a positive integer")
+        assert isinstance(page_size, int) and page_size > 0, (
+            "page_size must be a positive integer")
 
         dataset = self.dataset()
         start, end = index_range(page, page_size)
