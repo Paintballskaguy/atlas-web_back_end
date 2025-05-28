d0738c22624fe86688cd8d634978e2202b0abc08
Paintballskaguy <Johnstournament@gmail.com>
2025-05-28 13:29:41 -0500

    forgot to update the error message.

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -25,7 +25,7 @@ def not_found(error) -> str:
 def unauthorized(error) -> str:
     """ Unauthorized handler
     """
-    return jsonify({"error": "Not found"}), 401
+    return jsonify({"error": "Unauthorized"}), 401
 
 
 if __name__ == "__main__":
