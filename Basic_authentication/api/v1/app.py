3801a5ff33bf187ff2c9e28c89988fd4afbfc969
Paintballskaguy <Johnstournament@gmail.com>
2025-05-28 14:03:03 -0500

    trying the updated app.py with before_request

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -14,6 +14,31 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 
+auth_type = getenv('AUTH_TYPE')
+if auth_type == 'auth':
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+    
+@app.before_request
+def before_request():
+    """Filter each request before processing"""
+    if auth is None:
+        return
+
+    exclude_paths = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized/',
+        '/api/v1/forbidden/'
+    ]
+
+    if not auth.require_auth(request.path, exclude_paths):
+        return
+    if auth.authorization_header(request) is None:
+        abort(401)
+    if auth.current_user(request) is None:
+        abort(403)
+
+
 @app.errorhandler(404)
 def not_found(error) -> str:
     """ Not found handler
