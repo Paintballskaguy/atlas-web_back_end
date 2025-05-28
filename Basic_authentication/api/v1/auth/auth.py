e4453938483198c95eb0504f34bd4bae2ef7a365
Paintballskaguy <Johnstournament@gmail.com>
2025-05-28 13:53:22 -0500

    updated require_auth

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -19,7 +19,23 @@ class Auth:
         Returns:
             bool: False if path is in excluded_paths, True otherwise
         """
-        return False
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        # check that path ends in /
+        normalized_path = path if path.endswith('/') else path + '/'
+        
+        for excluded_path in excluded_paths:
+            if excluded_path.endswith('/'):
+                if normalized_path == excluded_path:
+                    return False
+            else:
+                if normalized_path == excluded_path + '/':
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Get the authorization header from the request
