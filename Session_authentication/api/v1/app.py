126f8615b7ef9ec66824a26d1922542cc85e4127
Paintballskaguy <Johnstournament@gmail.com>
2025-05-28 17:17:06 -0500

    added the login to excluded paths.

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -37,6 +37,7 @@ def before_request():
         '/api/v1/status/',
         '/api/v1/unauthorized/',
         '/api/v1/forbidden/'
+        '/api/v1/auth_session/login/'
     ]
 
     if not auth.require_auth(request.path, exclude_paths):
