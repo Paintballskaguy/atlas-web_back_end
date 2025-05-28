2cac05ac7ffb1797fb52f2539e96ba11ccc36271
Paintballskaguy <Johnstournament@gmail.com>
2025-05-28 16:06:10 -0500

    task 1 - updated users and app to reply to users/me

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -40,7 +40,8 @@ def before_request():
         return
     if auth.authorization_header(request) is None:
         abort(401)
-    if auth.current_user(request) is None:
+    request.current_user = auth.current_user(request)
+    if request.current_user is None:
         abort(403)
 
 
