c4402ee4e1765396ecd35a21205fa00e833539c6
Paintballskaguy <Johnstournament@gmail.com>
2025-05-28 17:07:47 -0500

    updated auth.py with session cookie function

--- Session_authentication/api/v1/auth/auth.py
+++ Session_authentication/api/v1/auth/auth.py
@@ -3,6 +3,7 @@
 """
 from flask import request
 from typing import List, TypeVar
+from os import getenv
 
 
 class Auth:
@@ -46,3 +47,11 @@ class Auth:
     def current_user(self, request=None) -> TypeVar('User'):
         """Get current user from request (always None in base Auth)"""
         return None
+
+    def session_cookie(self, request=None):
+        """Returns the value of session cookie from request"""
+        if request is None:
+            return None
+
+        session_name = getenv('SESSION_NAME', '_my_session_id')
+        return request.cookies.get(session_name)
