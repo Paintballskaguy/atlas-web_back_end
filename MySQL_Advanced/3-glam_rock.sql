36fe2d10983cb1d5272240edeb1c42d796c050a2
Paintballskaguy <Johnstournament@gmail.com>
2025-06-17 15:58:43 -0500

    LIST OF GLAM BANDS BY LONGIVITY

---
+++ MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,7 @@
+-- List of Glam Rock bands, ranked by lifespan
+
+SELECT band_name(IFNULL(split, YEAR(CURDATE()))) - formed AS lifespan
+
+FROM metal_bands
+WHERE style = 'Glam rock'
+ORDER BY lifespan DESC, band_name ASC;
