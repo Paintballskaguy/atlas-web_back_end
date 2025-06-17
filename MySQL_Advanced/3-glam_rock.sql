72e13b8feee1dfa20b30240b67cb80ca39df1a69
Paintballskaguy <Johnstournament@gmail.com>
2025-06-17 16:10:12 -0500

    CHANGED THE SELECT TO BE YEAR 2024... WHICH HAD BETTER NOT WORK

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- List of Glam Rock bands, ranked by lifespan
 
-SELECT band_name, (IFNULL(split, YEAR(CURDATE()))) - formed AS lifespan
+SELECT band_name, IFNULL (split, 2024) - formed AS lifespan
 
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
