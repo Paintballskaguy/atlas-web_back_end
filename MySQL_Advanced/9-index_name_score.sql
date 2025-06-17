ecaa55d7c287a469babdb7b3726bbb7d35f1bd1d
Paintballskaguy <Johnstournament@gmail.com>
2025-06-17 16:45:19 -0500

    removed , from the names

--- MySQL_Advanced/9-index_name_score.sql
+++ MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,3 @@
 -- Create a composite index on the first letter of a name and the score
 
-CREATE INDEX idx_name_first_score ON names, (name(1), score);
+CREATE INDEX idx_name_first_score ON names (name(1), score);
