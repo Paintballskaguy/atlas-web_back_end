12e7f5ee883a1d76f0baba96dc5e375c26d2a650
Paintballskaguy <Johnstournament@gmail.com>
2025-06-17 15:50:18 -0500

    SPELLING IS HARD

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- Ranks the country origins of bands by total number of fans
 
-SELECT origin, SUM(fans) AS NB_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
 ORDER BY nb_fans DESC;
